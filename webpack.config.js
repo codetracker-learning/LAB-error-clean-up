a301ac6c3e7e555fc2dfcf90ef9f8618567f97fb
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2022-09-15 14:38:08 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -64,6 +64,7 @@ module.exports = {
   },
   // Opens browser on run of npm start 
   devServer: {
-    open: true
+    open: true,
+    // allowedHosts: "all" // remove when not using on Gitpod
   }
 };
